bc881b18848a3fa4dd4d2de0af8d062b01789bbc
Alexander Gilbertson <alexandertgilbertson@gmail.com>
2024-02-23 12:03:03 -0600

    try adding screenshots

--- link.js
+++ link.js
@@ -1,19 +1,3 @@
-// Example URL
-let urlString;
-// index.html?url=some_value
-
-// Create a URL object
-let url = new URL(window.location.href);
-// Use window.location.href as the base URL
-
-// Get the value of the 'v' parameter
-let videoId = url.searchParams.get("v");
-console.log(videoId); // Output: 9Stt4wq3KCE
-
-let imageUrl = "https://i.ytimg.com/vi/" + videoId + "/maxresdefault.jpg";
-let ogTitle = "Watch on Youtube";
-let ogUrl = "https://www.youtube.com/watch?v=" + videoId;
-
 // Assuming you are working with the DOM in a browser environment
 function createMetaTag(property, content) {
   let metaTag = document.createElement("meta");
---
+++ videoID.js
@@ -0,0 +1,15 @@
+// Example URL
+let urlString;
+// index.html?url=some_value
+
+// Create a URL object
+let url = new URL(window.location.href);
+// Use window.location.href as the base URL
+
+// Get the value of the 'v' parameter
+let videoId = url.searchParams.get("v");
+console.log(videoId); // Output: 9Stt4wq3KCE
+
+let imageUrl = "https://i.ytimg.com/vi/" + videoId + "/maxresdefault.jpg";
+let ogTitle = "Watch on Youtube";
+let ogUrl = "https://www.youtube.com/watch?v=" + videoId;
